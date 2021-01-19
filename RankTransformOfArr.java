50a24104362d799351ce2a9387a0d0672e53c027
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-01-19 23:04:47 +0530

    Create RankTransformOfArr.java

---
+++ RankTransformOfArr.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] temp = new int[arr.length];
+        
+        for(int i = 0; i < arr.length; i++){
+            temp[i] = arr[i];
+        }
+        
+        Arrays.sort(temp);
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        int rank = 1;
+        for(int i = 0; i < temp.length; i++){
+            if(!map.containsKey(temp[i])){
+                map.put(temp[i], rank++);
+            }
+        }
+        
+        for(int i = 0; i < arr.length; i++){
+            temp[i] = map.get(arr[i]);
+        }
+     
+        return temp;
+    }
+}
