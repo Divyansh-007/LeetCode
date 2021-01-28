5b9e201d8a11c73fd57c106d0e7d442cdeb46bdf
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-01-28 21:33:24 +0530

    Create FindingGivenNodeInCloneOfABinaryTree.java

---
+++ FindingGivenNodeInCloneOfABinaryTree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if(original == null || original == target){
+            return cloned;
+        }
+        
+        TreeNode ans1 = getTargetCopy(original.left,cloned.left,target);
+        
+        if(ans1 != null){
+            return ans1;
+        }
+        
+        return getTargetCopy(original.right,cloned.right,target);
+    }
+}
