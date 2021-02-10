84a1629d256bc40b7cc54853036b14265a375cf4
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-02-10 16:54:00 +0530

    Create AverageSalaryExcludingTheMinimumAndMaximumSalary.java

---
+++ AverageSalaryExcludingTheMinimumAndMaximumSalary.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double average(int[] salary) {
+        Arrays.sort(salary);
+        
+        double total = 0;
+        for(int i = 1; i < salary.length - 1; i++){
+            total = total + salary[i];
+        }
+        
+        return total / (salary.length - 2);
+    }
+}
