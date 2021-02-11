defb7e65c310b3ccefde24024cee0c36c1b58208
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-02-11 11:38:15 +0530

    Create BestTimeToBuyAndSellStock_II.java

---
+++ BestTimeToBuyAndSellStock_II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i = 0;
+        int min = prices[0];
+        int max = prices[0];
+        int profit = 0;
+        
+        while(i < prices.length - 1){
+            while(i < prices.length - 1 && prices[i] >= prices[i + 1]){
+                i++;
+            }
+            
+            min = prices[i];
+            
+            while(i < prices.length - 1 && prices[i] <= prices[i + 1]){
+                i++;
+            }
+            
+            max = prices[i];
+            
+            profit = profit + (max - min);
+        }
+        
+        return profit;
+        
+    }
+}
