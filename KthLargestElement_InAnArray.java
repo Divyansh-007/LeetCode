ce18fd6be169a206d100137ac1ed6ab667b89975
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-02-19 11:33:11 +0530

    Create KthLargestElement_InAnArray.java

---
+++ KthLargestElement_InAnArray.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        
+        if(n == k){
+            Arrays.sort(nums);
+            return nums[0];
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i = 0; i < k; i++){
+            pq.add(nums[i]);
+        }
+        
+        for(int i = k ; i < n; i++){
+            if(pq.peek() < nums[i]){
+                pq.poll();
+                pq.add(nums[i]);
+            }
+        }
+        
+        return pq.peek();
+    }
+}
