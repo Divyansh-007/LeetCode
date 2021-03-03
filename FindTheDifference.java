a42d78768fdf2d48a0525e405bfbce3b97365d2b
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-03-03 14:27:30 +0530

    Create FindTheDifference.java

---
+++ FindTheDifference.java
@@ -0,0 +1,24 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] freq = new int[26];
+        
+        for(int i = 0; i < t.length(); i++){
+            freq[t.charAt(i) - 'a']++;
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+            freq[s.charAt(i) - 'a']--;
+        }
+        
+        int ind = 0;
+        
+        for(int i = 0; i < 26; i++){
+            if(freq[i] != 0){
+               ind = i;
+                break;
+            }
+        }
+        
+        return (char)('a' + ind);
+    }
+}
