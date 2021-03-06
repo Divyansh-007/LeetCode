54c1f8e48b21f25efc57adcf143a94cf09dc82fa
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-03-06 12:37:46 +0530

    Create ExcelSheetColumnTitle.java

---
+++ ExcelSheetColumnTitle.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String convertToTitle(int n) {
+        String ans = "";
+        
+        while(n > 0){
+            ans = (char)('A' + ((n - 1) % 26)) + ans;
+            n = (n - 1) / 26;
+        }
+        
+        return ans;
+    }
+}
